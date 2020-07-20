ee8f7fe5ad65535f61a490b1cdbb70376ded106a
ilyas chicha <ilyes.chicha@univ-msila.dz>
2020-07-20 13:16:43 +0100

    add the jQuery code to remove #landing section and open the #full-section

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 function entersite() {
+    $("#landing").css("transform", "translate(-200vw)");
+    $("#dimmed-bg").css("visibility", "visible");
+    $("#dimmed-bg").css("transform", "translate(0)");
+    $("#above-fold").css("opacity", "1");
+    $("#full-site").css("visibility", "visible");
+    $("#full-site").css("overflow", "visible");
+    $("#full-site").css("max-height", "auto");
+    $("#register-side-bar").css("transform", "translate(0)");
 }
